41dcb0197a06338220646f89d9e125de9124a2ae
Songhe Wang <songhewang@songhes-mbp.lan>
2021-07-23 13:46:10 -0400

    edit k_means.py

--- Code/k_means.py
+++ Code/k_means.py
@@ -2,21 +2,23 @@ import numpy as np
 
 def cal_distance(a,b):
   # a and b should be arrays
-  return 1
+  return euclidean_distance(a,b)
 
 def euclidean_distance(a,b):
-  return 1
+
+  return np.linalg.norm(np.array(a)-np.array(b))
 
 def transition_matrix_distance(a,b):
   #compute the average euclidean distance for each row in transition matrix
   return 1
 
 
-def k_means(points.cluster=2):
+def k_means(points,luster=2):
   cen_1 = points[0]
   cen_2 = points[-1]
   change = True
   while change:
+    print(cen_1,cen_2)
     cluster_1 = []
     cluster_2 = []
     #assign points to clusters
@@ -48,11 +50,20 @@ def k_means(points.cluster=2):
       if distance < temp:
         temp = distance
         new_cen_2 = p
-    if new_cen_1 == cen_1 and new_cen_2 == cen_2:
+    if (new_cen_1 == cen_1 and new_cen_2 == cen_2) or (new_cen_1 == cen_2 and new_cen_2 == cen_1):
       change = False
     else:
       cen_1 = new_cen_1
       cen_2 = new_cen_2
+
+  return cluster_1, cluster_2
+
+
+if __name__ == '__main__':
+  points = [[1,2],[3,4],[2,3],[100,100],[101,102]]
+  cen_1,cen_2 = k_means(points)
+  print(cen_1,cen_2)
+
 
 
 
